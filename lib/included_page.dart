832d5882c1c8d7a58f479db26ca6430c36d7b32f
F <f@thinkbitsolutions.com>
2021-09-19 20:18:33 +0800

    feat (included) : add title

---
+++ lib/included_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IncludedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [Text('Included in this template:')],
+      ),
+    );
+  }
+}

233154439a135def5f8f48894c889439746eded6
F <f@thinkbitsolutions.com>
2021-09-19 21:57:41 +0800

    fix (included_page) : center title

--- lib/included_page.dart
+++ lib/included_page.dart
@@ -5,9 +5,16 @@ class IncludedPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text('Included in this template:')],
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Included in this template:',
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
         ),
       ),
     );

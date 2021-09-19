930b6b755e2bbf6004d3cdb99947eee062916e52
F <f@thinkbitsolutions.com>
2021-09-19 23:07:20 +0800

    feat(localization) : center title of languages page

--- lib/languages_page.dart
+++ lib/languages_page.dart
@@ -4,15 +4,25 @@ class LanguagesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black.withOpacity(.8),
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Container(
-          child: Column(
-            children: [Text('Languages')],
+      backgroundColor: Colors.black.withOpacity(.70),
+      extendBody: true,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                    child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Text('Languages'),
+                ))
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
           ),
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(20)),
         ),
       ),
     );
